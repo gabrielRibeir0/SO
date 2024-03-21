20578685cf199fd01f1ac436f3f8e5e56aee003d
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2024-03-21 22:46:16 +0000

    Ficheiro ex da aula

--- Guiao_Exec/mysystem.c
+++ Guiao_Exec/mysystem.c
@@ -4,11 +4,48 @@
 // recebe um comando por argumento
 // returna -1 se o fork falhar
 // caso contrario retorna o valor do comando executado
-int mysystem (const char* command) {
+int mysystem(const char* command) {
 
+	int fork_ret, exec_ret, status;
 	int res = -1;
 
-	// TO DO
+	//estamos a assumir um nr maximo de argumentos, isso teria de ser melhorado com realloc
+	char* exec_args[20];
+	char* string;
+	char* cmd;
+	int i=0;
+
+	cmd = strdup(command);
+	while((string = strsep(&cmd, " ")) != NULL){
+		exec_args[i] = string;
+		i++;
+	}
+	exec_args[i] = NULL;
+
+	fork_ret = fork();
+
+	switch(fork_ret){
+		case -1:
+			res=-1;
+			break;
+		
+		case 0: //processo filho
+			exec_ret = execvp(exec_args[0], exec_args);
+			perror("erro no exec");
+			_exit(exec_ret);
+			break;
+		
+		default:
+			wait(&status);
+			if(WIFEXITED(status)==255){
+				res=-1;
+			}
+			else{
+				res = WEXITSTATUS(status);
+			}
+			break;
+	}
+	
 
 	return res;
 }
