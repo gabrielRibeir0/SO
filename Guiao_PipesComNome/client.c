c5e29b097afefd2a8090819ad8d01c2729e9f989
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2024-04-09 21:26:23 +0100

    Tentativa 1

--- Guiao_PipesComNome/client.c
+++ Guiao_PipesComNome/client.c
@@ -13,7 +13,21 @@ int main (int argc, char * argv[]){
 		_exit(1);
 	}
 
-	//TODO
+    int fd = open("FIFO_MATRIX", O_RDWR);
+    if(fd == -1){
+        perror("Erro na abertura do FIFO");
+        return -1;
+    }
+
+    int count;
+    int value = atoi(argv[1]);
+    write(fd, &value, sizeof(int));
+
+    ssize_t read_bytes;
+    while ((read_bytes = read(fd, &count, sizeof(int)))> 0){
+        printf("O valor %d foi encontrado %d vezes.\n", value, count);
+    }
+    unlink("FIFO_MATRIX");
 	
 	return 0;
 }
--- Guiao_PipesComNome/server.c
+++ Guiao_PipesComNome/server.c
@@ -10,11 +10,27 @@
 //Cliente pode receber um sigpipe (concorrência!)
 
 int main (int argc, char * argv[]){
-
 	init_vector();
 	print_vector();
 
-	//TODO
+    int fd = mkfifo("FIFO_MATRIX", 0666);
+    if(fd == -1){
+        perror("Erro ao criar o FIFO");
+        return -1;
+    }
+
+    fd = open("FIFO_MATRIX", O_RDWR);
+    if(fd == -1){
+        perror("Erro na abertura do FIFO");
+        return -1;
+    }
+
+    int value, count;
+    ssize_t read_bytes;
+    while ((read_bytes = read(fd, &value, sizeof(int)))> 0){
+        count = count_needle(value);
+        write(fd, &count, sizeof(int));
+    }
 	
 	return 0;
 }
